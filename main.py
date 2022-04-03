c7e6643b2661244136de859be0b3828ff4a2a0a0
shruti <shrutigupta5555@gmail.com>
2022-04-03 14:39:54 +0530

    display images

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from random import randint
 import discord 
 from discord.embeds import Embed 
 # from discord import client
@@ -7,10 +8,8 @@ from dotenv import load_dotenv
 import os
 
 
-bot = commands.Bot(command_prefix='!!')
+bot = commands.Bot(command_prefix='~')
 load_dotenv()
-
-
 
 @bot.command()
 async def ping(ctx):
@@ -18,8 +17,15 @@ async def ping(ctx):
 
 
 @bot.command()
-async def prompt(ctx):
-    await ctx.send_file('p')
+async def start(ctx):
+    await ctx.send('tag all the players playing the game 😏')
+
+    members = await bot.wait_for("message")
+    m = members.content.split(" ")
+
+    r = randint(0,30)
+    filepath = f"templates/{r}.jpg"
+    await ctx.send(file=discord.File(filepath))
 
 
 
