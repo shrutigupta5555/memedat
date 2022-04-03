c20d4aceae8453055d3f23198bd7b3431a31a698
shruti <shrutigupta5555@gmail.com>
2022-04-03 14:08:31 +0530

    add deepface

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ from discord.ext import commands
 from dotenv import load_dotenv 
 import os
 
+from deepface import DeepFace
+import numpy as np 
+
+from PIL import Image
 
 bot = commands.Bot(command_prefix='!!')
 load_dotenv()
 
 
+#detect emotions 
+
+def detect():
+    analyze = DeepFace.analyze(img_path="m.png", actions=['emotion'])  #here the first parameter is the image we want to analyze #the second one there is the action
+    return analyze['dominant_emotion']
 
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('pong pong')
+
+
+@bot.command()
+async def prompt(ctx):
+    await ctx.send('p')
+
+
+
 
 
 bot.run(os.getenv('BOT_TOKEN'))
